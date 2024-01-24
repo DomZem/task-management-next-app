fe700283b93a6b25aef3c01c83222c95ae45e46a
Dominik <dominik.zemlik.dev@gmail.com>
2024-01-24 17:37:59 +0100

    feat: create login form and add it to login page

--- app/(marketing)/(auth)/login/page.tsx
+++ app/(marketing)/(auth)/login/page.tsx
@@ -1,7 +1,11 @@
+import LoginForm from '@/components/LoginForm';
+
 export default function LoginPage() {
   return (
     <section className="w-full max-w-lg rounded-lg bg-white p-6 shadow-md dark:bg-primaryDarkGrey">
-      <h2 className="text-heading-xl mb-6">Login</h2>
+      <h2 className="mb-6 text-heading-xl">Login</h2>
+
+      <LoginForm />
     </section>
   );
 }
---
+++ components/LoginForm.tsx
@@ -0,0 +1,114 @@
+'use client';
+
+import { axiosInstance } from '@/lib/axios';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import { useRouter } from 'next/navigation';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { LuLoader2 } from 'react-icons/lu';
+import * as z from 'zod';
+import { Button } from './UI/Button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormInput,
+  FormInputWrapper,
+  FormItem,
+  FormLabel,
+} from './UI/Form';
+
+const loginFormSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(1, { message: 'Required' }),
+});
+
+export type Login = z.infer<typeof loginFormSchema>;
+
+const defaultValues: Login = {
+  email: '',
+  password: '',
+};
+
+const login = async (data: Login) => {
+  const response = await axiosInstance.post('/auth/login', data, {
+    withCredentials: true,
+  });
+
+  return response.data;
+};
+
+export default function LoginForm() {
+  const router = useRouter();
+  const form = useForm<Login>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues,
+  });
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: login,
+    onSuccess: () => {
+      router.push('/boards');
+    },
+    onError: (err: Error | AxiosError) => {},
+  });
+
+  const onSubmit: SubmitHandler<Login> = (data) => {
+    mutate(data);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        method="post"
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8"
+      >
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>address email</FormLabel>
+
+              <FormControl>
+                <FormInputWrapper>
+                  <FormInput {...field} />
+                </FormInputWrapper>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>password</FormLabel>
+
+              <FormControl>
+                <FormInputWrapper>
+                  <FormInput type="password" {...field} />
+                </FormInputWrapper>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <Button
+          className="flex w-full items-center justify-center"
+          type="submit"
+          disabled={isPending}
+          large
+        >
+          {isPending && (
+            <LuLoader2 className="mr-2 animate-spin text-base font-bold" />
+          )}
+          {isPending ? 'Loading ...' : 'Submit'}
+        </Button>
+      </form>
+    </Form>
+  );
+}
