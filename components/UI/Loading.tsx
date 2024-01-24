e91054d517345f984ec1a1089f52b89776badfc0
Dominik <dominik.zemlik.dev@gmail.com>
2024-01-24 18:17:50 +0100

    feat: create loading ui component

---
+++ components/UI/Loading.tsx
@@ -0,0 +1,7 @@
+import { LuLoader2 } from 'react-icons/lu';
+
+export default function Loading() {
+  return (
+    <LuLoader2 className="animate-spin text-4xl font-bold text-primaryPurple" />
+  );
+}
