fbc4ccd5d9db93f726a7521b189f36af9f1f7bf3
Dominik <dominik.zemlik.dev@gmail.com>
2024-01-24 17:45:52 +0100

    feat: create form schema for task and subtask

---
+++ components/TaskForm/formSchema.ts
@@ -0,0 +1,19 @@
+import * as z from 'zod';
+
+export const subtaskFormSchema = z.object({
+  id: z.number().default(0),
+  title: z.string().min(1, { message: "Can't be empty" }),
+  isComplete: z.boolean(),
+});
+
+export const taskFormSchema = z.object({
+  id: z.number().default(0),
+  title: z.string().min(1, { message: "Can't be empty" }).max(50),
+  description: z.string().optional(),
+  subtasks: z.array(subtaskFormSchema).optional(),
+  statusId: z.number(),
+});
+
+export type Task = z.infer<typeof taskFormSchema>;
+
+export type Subtask = z.infer<typeof subtaskFormSchema>;
