77e7c98549ced5c7e70e72afa2e6e707218a17ef
Dominik <dominik.zemlik.dev@gmail.com>
2024-01-24 17:49:42 +0100

    feat: create colors data

---
+++ data/colors.ts
@@ -0,0 +1,79 @@
+export interface Color {
+  name: string;
+  value: string;
+}
+
+export const colors: Color[] = [
+  {
+    value: '#9333ea',
+    name: 'Purple',
+  },
+  {
+    name: 'Slate',
+    value: '#475569',
+  },
+  {
+    name: 'Gray',
+    value: '#4b5563',
+  },
+  {
+    name: 'Zinc',
+    value: '#52525b',
+  },
+  {
+    name: 'Neutral',
+    value: '#525252',
+  },
+  {
+    name: 'Stone',
+    value: '#57534e',
+  },
+  {
+    name: 'Red',
+    value: '#dc2626',
+  },
+  {
+    name: 'Orange',
+    value: '#ea580c',
+  },
+  {
+    name: 'Amber',
+    value: '#d97706',
+  },
+  {
+    name: 'Yellow',
+    value: '#ca8a04',
+  },
+  {
+    name: 'Lime',
+    value: '#65a30d',
+  },
+  {
+    name: 'Green',
+    value: '#16a34a',
+  },
+  {
+    value: '#2563eb',
+    name: 'Blue',
+  },
+  {
+    value: '#4f46e5',
+    name: 'Indigo',
+  },
+  {
+    value: '#7c3aed',
+    name: 'Violet',
+  },
+  {
+    value: '#c026d3',
+    name: 'Fuchsia',
+  },
+  {
+    value: '#db2777',
+    name: 'Pink',
+  },
+  {
+    value: '#e11d48',
+    name: 'Rose',
+  },
+];
