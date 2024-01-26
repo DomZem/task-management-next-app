f73bc8311b9845b2a6f4b8e0486470bde2d61377
Dominik <dominik.zemlik.dev@gmail.com>
2024-01-26 22:57:56 +0100

    feat(toaster): change font size and colors in dark mode

--- components/UI/Toaster.tsx
+++ components/UI/Toaster.tsx
@@ -1,13 +1,18 @@
 'use client';
 
+import { useTheme } from 'next-themes';
 import { Toaster as ReactToaster } from 'react-hot-toast';
 
 export default function Toaster() {
+  const { theme } = useTheme();
+
   return (
     <ReactToaster
       toastOptions={{
         style: {
-          fontSize: '15px',
+          backgroundColor: theme === 'dark' ? '#20212c' : '#ffffff',
+          color: theme === 'dark' ? '#ffffff' : '#20212c',
+          fontSize: '13px',
           fontWeight: 500,
           maxWidth: '400px',
         },
