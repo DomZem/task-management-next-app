5314df893000211e874817ec97e203b09bb5055c
Dominik <dominik.zemlik.dev@gmail.com>
2024-01-26 20:48:07 +0100

    feat(task list): create show up animation using framer motion

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import { Task } from './TaskForm/formSchema';
 import TaskListItem from './TaskListItem';
 
@@ -5,12 +6,29 @@ interface TaskListProps {
   tasks: Omit<Task, 'subtasks'>[];
 }
 
+const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
 export default function TaskList({ tasks }: TaskListProps) {
   return (
-    <ul className="flex flex-col gap-5">
+    <motion.ul
+      className="flex flex-col gap-5"
+      variants={container}
+      initial="hidden"
+      animate="visible"
+    >
       {tasks.map((task) => (
         <TaskListItem task={task} key={task.id} />
       ))}
-    </ul>
+    </motion.ul>
   );
 }
--- components/TaskListItem.tsx
+++ components/TaskListItem.tsx
@@ -1,10 +1,19 @@
 import useSubtasksQuery from '@/hooks/useSubtasksQuery';
 import { getCompletedSubtasksAmount } from '@/lib/utils';
+import { motion } from 'framer-motion';
 import { Task } from './TaskForm/formSchema';
 
 interface TaskListItemProps {
   task: Omit<Task, 'subtasks'>;
 }
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
 
 export default function TaskListItem({ task }: TaskListItemProps) {
   const { data: subtasks, isLoading, error } = useSubtasksQuery(task.id);
@@ -18,13 +27,16 @@ export default function TaskListItem({ task }: TaskListItemProps) {
   }
 
   return (
-    <li className="cursor-pointer rounded-lg bg-primaryWhite px-4 py-[23px] shadow-task outline-none transition-shadow duration-200 hover:shadow-taskHover dark:bg-primaryDarkGrey">
+    <motion.li
+      className="cursor-pointer rounded-lg bg-primaryWhite px-4 py-[23px] shadow-task outline-none transition-shadow duration-200 hover:shadow-taskHover dark:bg-primaryDarkGrey"
+      variants={item}
+    >
       <h4 className="mb-2 break-words text-heading-m text-primaryBlack dark:text-primaryWhite">
         {task.title}
       </h4>
       <p className="text-body-m text-primaryMediumGrey">
         {getCompletedSubtasksAmount(subtasks)} of {subtasks.length} subtasks
       </p>
-    </li>
+    </motion.li>
   );
 }
