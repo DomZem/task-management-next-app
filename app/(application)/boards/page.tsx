da0dca511a68a328e381cb9cddfa6fe613c0a15b
Dominik <dominik.zemlik.dev@gmail.com>
2024-01-30 21:24:02 +0100

    feat(boards page): add mobile nav to header

--- app/(application)/boards/page.tsx
+++ app/(application)/boards/page.tsx
@@ -1,4 +1,5 @@
 import CreateBoardForm from '@/components/BoardForm/CreateBoardForm';
+import MobileNav from '@/components/MobileNav';
 import { Button } from '@/components/UI/Button';
 import { DialogTrigger } from '@/components/UI/Dialog';
 import logoImage from '@/public/logo.svg';
@@ -7,14 +8,19 @@ import Image from 'next/image';
 export default function BoardsPage() {
   return (
     <>
-      <header className="flex h-16 items-center bg-white px-4 md:hidden">
-        <div className="flex items-center gap-4">
+      <header className="flex h-16 items-center bg-white px-4 dark:bg-primaryDarkGrey md:hidden">
+        <section className="flex items-center gap-4">
           <Image src={logoImage} alt="logo" />
-          <h1 className="text-heading-l">Kanban</h1>
-        </div>
+
+          <div className="flex items-center gap-2">
+            <h1 className="text-heading-l">Kanban</h1>
+
+            <MobileNav />
+          </div>
+        </section>
       </header>
 
-      <main className="flex flex-col items-center justify-center gap-6">
+      <main className="flex h-full flex-col items-center justify-center gap-6">
         <h2 className="text-heading-m">Hello, User Name</h2>
 
         <CreateBoardForm>
