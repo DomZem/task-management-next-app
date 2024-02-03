d590c6cf91a40f40539400f866a12e66d703536d
Dominik <dominik.zemlik.dev@gmail.com>
2024-02-03 16:58:57 +0100

    feat(board title): add skeleton to loading state

--- components/BoardTitle.tsx
+++ components/BoardTitle.tsx
@@ -3,12 +3,13 @@
 import useBoard from '@/hooks/board/useBoard';
 import { handleError } from '@/lib/axios';
 import { notFound } from 'next/navigation';
+import { Skeleton } from './UI/Skeleton';
 
 export default function BoardTitle() {
   const { data: board, isLoading, error } = useBoard();
 
   if (isLoading) {
-    return <h2 className="text-heading-l">loading ...</h2>;
+    return <Skeleton className="h-[15px] w-[150px]" />;
   }
 
   if (error) {
